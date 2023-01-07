b49c85492e413aaf5e60bcbe439137a37af1e0f0
NupurGohite01 <118847362+NupurGohite01@users.noreply.github.com>
2023-01-07 12:41:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
 
+// const menu = document.getElementById('menu');
+// const a =document.getElementsByTagName("a");
+
+// a.addEventListener('click', function() {
+//   if (menu.style.display === 'block') {
+//     menu.style.display = 'none';
+//   } else {
+//     menu.style.display = 'block';
+//   }
+// });
